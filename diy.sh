d9d1d481ecf2dfc18398309874ee88d266764353
chrishtian <158126731+chrishtian@users.noreply.github.com>
2024-11-17 21:03:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,10 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+git clone https://github.com/kenzok8/small-package
+rm -rfv feeds/luci/applications/luci-app-passwall
+mv small-package/luci-app-passwall feeds/luci/applications/
+rm -rfv feeds/packages/net/mwan3
+mv small-package/mwan3 feeds/packages/net/
+rm -rfv feeds/luci/applications/luci-app-lucky
+mv small-package/luci-app-lucky feeds/luci/applications/
