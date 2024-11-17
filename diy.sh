af834b207fbcd7bcfa369cffc1a6bff8f0f6b764
chrishtian <158126731+chrishtian@users.noreply.github.com>
2024-11-17 21:43:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,7 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-cd immortalwrt
-git clone https://github.com/kenzok8/small-package
+git clone https://github.com/kenzok8/small-package ImmortalWrt/small-package
 rm -rfv feeds/luci/applications/luci-app-passwall
 mv small-package/luci-app-passwall feeds/luci/applications/
 rm -rfv feeds/packages/net/mwan3
